764595c25b8949de511d1e734c73cdcff5c9b480
beckg <beck.gabor@gmail.com>
2020-12-10 16:40:26 +0100

    viszem haza

--- src/main/java/trackingorder/Stock.java
+++ src/main/java/trackingorder/Stock.java
@@ -2,6 +2,7 @@ package trackingorder;
 
 public class Stock extends Product {
     private Integer amount;
+    private Integer foglalva;
 
     public Stock(Integer id, String name, Double price, Integer amount) {
         super(id, name, price);
@@ -12,4 +13,21 @@ public class Stock extends Product {
         super( product.getId(),product.getName(), product.getPrice());
         this.amount = amount;
     }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    public Integer getFoglalva() {
+        return foglalva;
+    }
+
+    public void setFoglalva(Integer foglalva) {
+        this.foglalva = foglalva;
+    }
+
 }
--- src/main/java/trackingorder/WareHouse.java
+++ src/main/java/trackingorder/WareHouse.java
@@ -1,28 +1,40 @@
 package trackingorder;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class WareHouse {
-    private List<Stock> stock = new ArrayList<>();
+    private HashMap<Integer,Stock> stock = new HashMap<>();
 
     private Stock findStockById(Integer id){
+        Stock s;
+
+        s = stock.get( id);
+
         return null;
     }
 
     public void addTermek( Product product , Integer amount ){
-        stock.add(  new Stock( product, amount ) );
+        stock.put( product.getId() ,  new Stock( product, amount ) );
     }
 
     public void plusKeszlet( Integer id, Integer darab ){
+    }
+
+    public void minusKeszlet( Integer id, Integer darab ){
 
     }
 
     public Integer getKeszlet( Integer id ){
-        return 0;
+        return findStockById( id ).getAmount();
     }
 
-    public void minusKeszlet( Integer id, Integer darab ){
+    public void foglal( Integer id, Integer darab){
+
+    }
+
+    public void felszabadit( Integer id, Integer darab){
 
     }
 
